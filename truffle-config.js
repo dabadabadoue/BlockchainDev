1432e4c7bdf372ad5042158ff63fd79436fbdd77
Daniel Abdoue <dabadabadoue@gmail.com>
2021-07-05 16:35:08 -0500

    editing config to remove ganache dev network

--- truffle-config.js
+++ truffle-config.js
@@ -122,10 +122,10 @@ module.exports = {
     }
   },
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
-    }
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "*",
+    // }
   }
 }
